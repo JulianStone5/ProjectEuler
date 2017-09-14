503c209b813b13f2d8c4bb811fbd095f7c776157
Julian <Julian@10.27.66.128>
2017-09-14 15:12:28 -0400

    finished problem 14

---
+++ src/prob14.java
@@ -0,0 +1,32 @@
+public class prob14 {
+	
+	public static void main(String[] args) {
+		int maxSteps = 0;
+		long num = 0;
+		for(long i = 999999; i > 19; i--) {
+			long n = i;
+			int steps = 0;
+			while(n != 1) {
+				if(n % 2 == 0) {
+					n = nIsEven(n);
+				}
+				else n = nIsOdd(n);
+				steps++;
+			}
+			if(steps > maxSteps) {
+				num = i;
+				maxSteps = steps;
+			}
+		}
+		System.out.println(num);
+	}
+	
+	public static long nIsEven(long n) {
+		return n / 2;
+	}
+	
+	public static long nIsOdd(long n) {
+		return 3 * n + 1;
+	}
+	
+}
