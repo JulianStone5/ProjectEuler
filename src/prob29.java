90e5e1b01539ea7de2b1ac5549c357037f02b0a3
JulianStone5 <julian.stone@students.olin.edu>
2017-09-15 16:07:38 -0400

    Finished problem 29

---
+++ src/prob29.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class prob29 {
+	
+	public static ArrayList<BigInteger> powers = new ArrayList<BigInteger>();
+	
+	public static void main(String[] args) {
+		for(int a = 2; a <= 100; a++) {
+			for(int b = 2; b <= 100; b++) {
+				BigInteger A = new BigInteger("" + a);
+				A = A.pow(b);
+				if(!powers.contains(A)) powers.add(A);
+			}
+		}
+		System.out.println(powers.size());
+	}
+	
+}
