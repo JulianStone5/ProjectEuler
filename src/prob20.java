08687e15400c64ad3cd8a79a87fdc1a58df52422
JulianStone5 <julian.stone@students.olin.edu>
2017-09-18 15:38:52 -0400

    Finished problem 20

--- src/prob20.java
+++ src/prob20.java
@@ -4,6 +4,15 @@ public class prob20 {
 	
 	public static void main(String[] args) {
 		BigInteger factorial = BigInteger.ONE;
+		for(int i = 100; i >= 2; i--) {
+			factorial = factorial.multiply(new BigInteger("" + i));
+		}
+		String num = factorial.toString();
+		int sum = 0;
+		for(int i = 0; i < num.length(); i++) {
+			sum += Integer.parseInt("" + num.charAt(i));
+		}
+		System.out.println(sum);
 	}
 	
 }
