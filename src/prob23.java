c99c6875b6e390a30153663a0848bf5b25c659d6
Julian <Julian@10.27.66.128>
2017-09-19 13:08:35 -0400

    Finished problem 23

---
+++ src/prob23.java
@@ -0,0 +1,31 @@
+public class prob23 {
+	
+	public static void main(String[] args) {
+		boolean[] isAb = new boolean[28124];
+		for(int i = 1; i < 28124; i++) {
+			isAb[i] = isAbundant(i);
+		}
+		int sum = 0;
+		for(int i = 1; i < 28124; i++) {
+			if(!isSum2Ab(isAb, i)) sum += i;
+		}
+		System.out.println(sum);
+	}
+	
+	public static boolean isSum2Ab(boolean[] isAb, int n) {
+		for(int i = 1; i <= n/2; i++) {
+			if(isAb[i] && isAb[n-i]) return true;
+		}
+		return false;
+	}
+	
+	public static boolean isAbundant(int n) {
+		int sum = 1;
+		for(int i = 2; i*i <= n; i++) {
+			if(n % i == 0) sum += i + n / i;
+			if(n % i == 0 && i*i == n) sum -= i;
+		}
+		return sum > n;
+	}
+	
+}
