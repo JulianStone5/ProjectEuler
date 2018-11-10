772bed4873629445eb5faad6c2a3f0620b527d3a
jstone <jstone@JTRZ7H2.olin.edu>
2018-11-09 20:03:44 -0500

    finished problem 27

--- src/prob027.java
+++ src/prob027.java
@@ -2,11 +2,11 @@ public class prob027 {
 	
 	public static void main(String[] args) {
 		int aMax = 0, bMax = 0, nMax = 0;
-		for(int a = -999; a < 1000; a += 2) {
-			for(int b = 3; b <= 1000; b += 2) {
+		for(int a = -999; a < 1000; a++) {
+			for(int b = 0; b <= 1000; b++) {
 				if(isPrime(b)) {
 					int n = 0;
-					while(isPrime(Math.abs(n^2 + a*n + b))) {
+					while(isPrime(n*n + a*n + b)) {
 						n++;
 					}
 					if(n > nMax) {
@@ -17,14 +17,16 @@ public class prob027 {
 				}
 			}
 		}
-		System.out.println(bMax);
+		System.out.println(aMax*bMax);
 	}
 	
 	public static boolean isPrime(int n) {
+		if(n<2) {
+			return false;
+		}
 		for(int i = 2; i <= Math.sqrt(n); i++) {
 			if(n % i == 0) return false;
 		}
 		return true;
 	}
-	
 }
